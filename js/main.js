acd9ad6be1b36ce144d01fff694dfc73de063e3d
amr-khllaf <amr.a.khllaf@gmail.com>
2025-01-07 20:02:21 +0200

    add player elements and implement active player switching logic in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //* Selecting Elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1"); //! This is a liittle bit faster than query selector()
 const current0El = document.getElementById("current--0");
@@ -16,7 +18,10 @@ score0El.textContent = 0;
 console.log(typeof score0El.textContent); //^ String ==> cause js will convert them to string to show them in screen
 score1El.textContent = 0;
 diceEl.classList.add("hidden"); //! To hide Dice at the start of game beginning
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 //* ==> rolling Dice Functionality
 btnRoll.addEventListener("click", function () {
   //! 1. Generating a random dice roll
@@ -31,8 +36,16 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     //! Add The Dice To Current Score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    //* This a trick to build id dynamically
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore;
   } else {
     //! switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; //* Reassign the active player
+    currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
