be48d36d444de05b2da62f1e6ee6107950845871
amr-khllaf <amr.a.khllaf@gmail.com>
2025-01-07 19:36:44 +0200

    add rolling dice functionality and initialize current score elements in main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
 "use strict";
 
-//! Selecting Elements
+//* Selecting Elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1"); //! This is a liittle bit faster than query selector()
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//* starting Condtitions
 score0El.textContent = 0;
 console.log(typeof score0El.textContent); //^ String ==> cause js will convert them to string to show them in screen
 score1El.textContent = 0;
 diceEl.classList.add("hidden"); //! To hide Dice at the start of game beginning
+let currentScore = 0;
+//* ==> rolling Dice Functionality
+btnRoll.addEventListener("click", function () {
+  //! 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //! 2. Display Dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `./imgs/dice-${dice}.png`; //^ Here is the trick to pick one img from the 6 img via src attribute using ``
+
+  //! 3. Check for rolled ==> 1: if true
+  if (dice !== 1) {
+    //! Add The Dice To Current Score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //! switch to next player
+  }
+});
